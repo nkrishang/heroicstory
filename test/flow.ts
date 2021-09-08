02ce2934e25cd57167660f94d36f90fd82541f46
nkrishang <kn1457@nyu.edu>
2021-09-08 14:45:17 -0400

    added test for collecting multiple payouts

--- test/flow.ts
+++ test/flow.ts
@@ -79,7 +79,7 @@ describe("Testing entire basic flow", function () {
       await heroicStory.mintTo(await owner.getAddress(), "dummyuri", lootAddress, lootTokenId);
     })
 
-    it("Should distribute payouts correctly", async () => {
+    it("Should distribute a single payout correctly", async () => {
 
       const amount = ethers.utils.parseEther("100.0")
 
@@ -109,6 +109,52 @@ describe("Testing entire basic flow", function () {
           expect(tokenId).to.equal(nftTokenId)
           expect(shares).to.equal(2000)
           expect(amountPaid).to.equal((amount.mul(2000)).div(10000))
+
+          resolve(null)
+        })
+
+        setTimeout(() => {
+          reject(new Error("Timeout: SharesCollected"))
+        }, 10000)
+      })
+
+      await heroicStory.connect(contributors[0]).collectPayout(nftTokenId)
+      await SharesCollectedPromise
+    })
+
+    it("Should distribute multiple payouts correclty", async () => {
+      const amount = ethers.utils.parseEther("100.0")
+
+      // Assign shares.
+      await heroicStory.updateResults(
+        nftTokenId,
+        // 4 contributors.
+        [
+          await contributors[0].getAddress(), 
+          await contributors[1].getAddress(), 
+          await contributors[2].getAddress(), 
+          await contributors[3].getAddress()
+        ],
+        // Shares - [20%, 10%, 5%, 2.5%] 
+        [2000, 1000, 500, 250]
+      )
+      
+      // Update pool amount three times.
+      const numPayoutsToCollect: number = 3;
+
+      for(let i = 0; i < numPayoutsToCollect; i++) {
+        await heroicStory.updatePool(nftTokenId, amount, {
+          value: amount,
+        })
+      }
+
+      const SharesCollectedPromise = new Promise((resolve, reject) => {
+        heroicStory.on("SharesCollected", async (sender, tokenId, shares, amountPaid) => {
+
+          expect(sender).to.equal(await contributors[0].getAddress())
+          expect(tokenId).to.equal(nftTokenId)
+          expect(shares).to.equal(2000)
+          expect(amountPaid).to.equal(((amount.mul(2000)).div(10000)).mul(numPayoutsToCollect))
 
           resolve(null)
         })
